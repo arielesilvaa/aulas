730cc998ae5655f4ed5cfda2c8b84fb2ada52f86
Ariele Silva <139187649+arielesilvaa@users.noreply.github.com>
2025-02-17 15:39:42 -0300

    aula 37 ok

---
+++ aula37.js
@@ -0,0 +1,6 @@
+const caixa1 = document.querySelector('.caixa1');
+//const btn_c1 = document.querySelector('#c1');
+
+caixa1.addEventListener('click', ()=>{
+    console.log('Clicou na caixa 1');
+})
