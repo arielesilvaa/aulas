1475788e972531e788cd82dbf656162b47b92448
Ariele Silva <139187649+arielesilvaa@users.noreply.github.com>
2025-02-24 10:45:41 -0300

    parte 2

--- aula38.js
+++ aula38.js
@@ -2,4 +2,10 @@ const caixa1 = document.querySelector('#caixa1');
 const btn_c = [...document.querySelectorAll('.curso')];
 
 
-console.log(caixa1.children);
+console.log(caixa1.haschildnodes());
+console.log(btn_c[0].hasChildnodes());
+console.log(btn_c[0].Childnodes());
+
+
+console.log(btn_c[0].haschildnodes > 0 ? 'Tem filhos' : 'Não tem filhos');
+
